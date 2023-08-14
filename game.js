b448427ba30b9968efea1ba87411e94e51418e73
Naomi Robinson <Nao.Robinson7@gmail.com>
2023-08-14 20:08:20 -0300

    ganar, perder y agregar obstaculos

--- game.js
+++ game.js
@@ -6,13 +6,12 @@ export default class Game extends Phaser.Scene {
   
     init() {
 
-        this.perder = false;
         this.nivel = 1;
-
         this.choque = 0;
+        this.puntos = 0;
 
     }
-  
+
     preload() {
 
         this.load.image("plataforma", "./assets/plataforma.png");
@@ -20,21 +19,25 @@ export default class Game extends Phaser.Scene {
         this.load.image("obstaculo", "./assets/obstaculo.png");
 
     }
-  
+
     create() {
 
         this.plataforma = this.physics.add.sprite(400, 500, "plataforma").setScale (0.4).refreshBody();
-        this.pelota = this.physics.add.sprite(400, 300, "pelota").setScale (0.5);
+        this.pelota = this.physics.add.sprite(200, 200, "pelota").setScale (0.5);
+
+        this.physics.world.setBoundsCollision(true, true, true, false);
+
+        this.grupoObstaculos = this.physics.add.staticGroup();
 
         this.plataforma.setCollideWorldBounds(true);
         this.plataforma.body.setAllowGravity(false);
-        this.plataforma.body.setImmovable(true);
-
-        
+        this.plataforma.setBounce(1);
+ 
         this.pelota.setCollideWorldBounds(true);
         this.pelota.setBounce(1);
         this.pelota.body.setCircle(25);
         this.pelota.body.setFriction(0);
+        this.pelota.reboteBloqueado = false;
 
 
 
@@ -46,22 +49,37 @@ export default class Game extends Phaser.Scene {
             this
             );
 
+            this.physics.add.collider(
+                this.pelota,
+                this.grupoObstaculos,
+                );
+
+            this.physics.add.collider(
+                this.grupoObstaculos,
+                this.plataforma,
+            );
+
         this.cursors = this.input.keyboard.createCursorKeys();
         this.velocidadPlataforma = 500;
         this.velocidadPelota = 300;
 
-        this.pelota.setVelocity(this.velocidadPelota, - this.velocidadPelota);
+        this.pelota.setVelocity(this.velocidadPelota, this.velocidadPelota);
 
-        this.textoNivel = this.add.text(16, 40, "Nivel:" + this.nivel, {
+        this.textoNivel = this.add.text(16, 20, "Nivel:" + this.nivel, {
             fontSize: "20px",
             fill: "#FFFFFF",
             fontStyle: "bold",
         });
 
+        this.textoPuntos = this.add.text(16, 40, "Puntaje:" + this.puntos, {
+            fontSize: "20px",
+            fill: "#FFFFFF",
+            fontStyle: "bold",
+        });
 
-  }
-  
-  update() {
+}
+
+update() {
     if (this.cursors.left.isDown) {
         this.plataforma.setVelocityX(-this.velocidadPlataforma);
     } else if (this.cursors.right.isDown) {
@@ -70,54 +88,125 @@ export default class Game extends Phaser.Scene {
         this.plataforma.setVelocityX(0);
     }
 
-    // if (this.cursors.up.isDown) {
-    //     this.plataforma.setVelocityY(-this.velocidadPlataforma);
-    // } else if (this.cursors.down.isDown) {
-    //     this.plataforma.setVelocityY(this.velocidadPlataforma);
-    // } else {
-    // this.plataforma.setVelocityY(0);
-    // }
+    if (this.cursors.up.isDown && this.plataforma.y > 300) {
+        this.plataforma.setVelocityY(-this.velocidadPlataforma);
+    } else if (this.cursors.down.isDown && this.plataforma.y < this.cameras.main.height - 50) {
+        this.plataforma.setVelocityY(this.velocidadPlataforma);
+    } else {
+        this.plataforma.setVelocityY(0);
 
-  }
-
-    sumarPuntos(_pelota,_plataforma) {
-
-        this.choque = this.choque + 1;
-
-        console.log ("choque " + this.choque)
-
-        if (this.choque == 10) {
-            this.choque = 0;
-            this.pasarNivel();
-        }
-
+    if (this.pelota.reboteBloqueado) {
+        this.pelota.setVelocity(0, -this.velocidadPelota);
+        this.pelota.reboteBloqueado = false; // Permitir otro rebote después de un choque
     }
 
-  pasarNivel() {
-
-    console.log ("pasarNivel");
-
-    if (this.nivel == 21) {
-        this.ganar();
+    if (this.pelota.y > this.cameras.main.height) {
+        this.perder();
     }
+    }
+
+}
+
+sumarPuntos(_pelota,_plataforma) {
+
+    if (!this.pelota.reboteBloqueado) {
+        this.pelota.body.setVelocityX(this.pelota.body.velocity.x);
+        this.pelota.body.setVelocityY(-this.velocidadPelota);
+        this.pelota.reboteBloqueado = true; 
+    }
+
+    this.choque = this.choque + 1;
+
+    this.puntos = this.puntos + 10;
+
+    console.log ("choque " + this.choque)
+    this.textoPuntos.setText("Puntaje:" + this.puntos);
+
+    if (this.choque == 3) {
+        this.choque = 0;
+        this.pasarNivel();
+    }
+
+}
+
+pasarNivel() {
+
+    console.log ("paso de Nivel");
+
 
     const colorAleatorio = Phaser.Display.Color.RandomRGB();
-        this.cameras.main.setBackgroundColor(colorAleatorio.color);
+    this.cameras.main.setBackgroundColor(colorAleatorio.color);
 
     this.nivel++;
     this.textoNivel.setText("Nivel: " + this.nivel);
 
     this.velocidadPelota *= 1.1;
 
+    this.agregarObstaculo();
 
-  }
-
-  agregarObstaculo() {}
-
-  perder(){}
-
-  ganar(){}
+    if (this.nivel == 3) {
+        this.ganar();
+    }
+    
 
 
+}
+
+agregarObstaculo() {
+    const x = Phaser.Math.Between(50, 750);
+    const y = Phaser.Math.Between(100, 400);
+    const scale = Phaser.Math.FloatBetween(0.3, 0.8);
+    const obstaculo = this.physics.add.staticSprite(x, y, "obstaculo").setScale(scale).refreshBody();
+    
+    this.grupoObstaculos.add(obstaculo);
+}
+
+perder(){
+    this.grupoObstaculos.getChildren().forEach(obstaculo => {
+        obstaculo.disableBody(true, true);
+    });
+    this.plataforma.disableBody(true, true);
+    this.pelota.disableBody(true, true);
+
+    this.textoPerder = this.add.text(155, 200, "Has perdido", {
+        fontSize: "70px",
+        fill: "#FFFFFF",
+        // fontStyle: "bold",
+    });
+
+    this.textoPerder = this.add.text(60, 300, "reinicia la página para intentarlo de nuevo ", {
+        fontSize: "25px",
+        fill: "#FFFFFF",
+        // fontStyle: "bold",
+    });
+
+    this.textoNivel.destroy();
+    this.textoPuntos.destroy();
+    
+
+}
+
+ganar(){
+    this.grupoObstaculos.getChildren().forEach(obstaculo => {
+        obstaculo.disableBody(true, true);
+    });
+    this.plataforma.disableBody(true, true);
+    this.pelota.disableBody(true, true);
+
+    this.textoPerder = this.add.text(100, 200, "FELICITACIONES", {
+        fontSize: "70px",
+        fill: "#FFFFFF",
+        // fontStyle: "bold",
+    });
+
+    this.textoPerder = this.add.text(100, 300, "reinicia la página para jugar de nuevo ", {
+        fontSize: "25px",
+        fill: "#FFFFFF",
+        // fontStyle: "bold",
+    });
+
+    this.textoNivel.destroy();
+    this.textoPuntos.destroy();
+}
 
 }
